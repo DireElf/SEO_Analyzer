e823e2e3a7a49159d9026207c1b4d78e932d1454
DireElf <3672@mail.ru>
2022-10-13 10:54:31 +0700

    fix railway issues

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -11,7 +11,7 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "5000");
-        return Integer.parseInt(port);
+        return Integer.valueOf(port);
     }
 
     private static void addRoutes(Javalin app) {
