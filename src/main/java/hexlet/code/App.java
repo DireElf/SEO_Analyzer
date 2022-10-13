ec1dbd45fbf28ad3abe6e8608539a118dd048a1c
DireElf <3672@mail.ru>
2022-10-13 11:07:22 +0700

    port changed

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -10,7 +10,7 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "5000");
+        String port = System.getenv().getOrDefault("PORT", "3000");
         return Integer.valueOf(port);
     }
 
